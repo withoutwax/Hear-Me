f5af4d2a01bfaec1461ae9dba7c3a4539a5286a4
Will <rlagmlckd@gmail.com>
2020-01-16 21:47:32 -0600

    Initial Commit

---
+++ Hear Me/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hear Me
+//
+//  Created by Will Kim on 1/16/20.
+//  Copyright © 2020 Will Kim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hear Me/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hear Me
+//
+//  Created by Will Kim on 1/16/20.
+//  Copyright © 2020 Will Kim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
